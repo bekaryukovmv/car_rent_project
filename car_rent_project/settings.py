5cb646bc9e0bf91167cfa009ba7dbc4e43996b0a
Maxim <bekaryukovmv@gmail.com>
2019-11-14 21:30:17 +0700

    add static settings improve base.html

--- car_rent_project/settings.py
+++ car_rent_project/settings.py
@@ -142,6 +142,14 @@ LOCALE_PATHS = (
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+"django.contrib.staticfiles.finders.FileSystemFinder",
+"django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+
 
 AUTH_USER_MODEL = 'users.CustomUser'
 

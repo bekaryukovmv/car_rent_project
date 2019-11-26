3774370986d7e40f86df80df14323ad5ea95d8bd
Maxim Bekaryukov <bekaryukovmv@gmail.com>
2019-11-26 21:49:48 +0700

    added form tests for users

--- users/tests/test_forms.py
+++ users/tests/test_forms.py
@@ -1,4 +1,4 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
 from django.utils.translation import activate
 
 from users.forms import UserFormForEdit, CustomUserCreationForm, MyCustomSignupForm, CustomUserChangeForm
@@ -33,7 +33,6 @@ class CustomUserCreationFormTest(SimpleTestCase):
 
     def test_create_user_rus_form_fields_label(self):
         activate('ru')
-        # print(self.form.fields['username'].label)
         self.assertTrue(self.form.fields['email'].label == None or self.form.fields['email'].label == 'Email')
         self.assertTrue(self.form.fields['username'].label == None or self.form.fields['username'].label == 'Имя пользователя')
         self.assertTrue(self.form.fields['user_lang'].label == None or self.form.fields['user_lang'].label == 'Язык')
@@ -51,7 +50,6 @@ class CustomUserChangeFormTest(SimpleTestCase):
     
     def test_user_change_rus_form_fields_label(self):
         activate('ru')
-        # print(self.form.fields['username'].label)
         self.assertTrue(self.form.fields['email'].label == None or self.form.fields['email'].label == 'Email')
         self.assertTrue(self.form.fields['username'].label == None or self.form.fields['username'].label == 'Имя пользователя')
         self.assertTrue(self.form.fields['user_lang'].label == None or self.form.fields['user_lang'].label == 'Язык')
@@ -63,9 +61,46 @@ class CustomUserChangeFormTest(SimpleTestCase):
         self.assertTrue(self.form.fields['user_lang'].label == None or self.form.fields['user_lang'].label == 'Language')
 
 
-class MyCustomSignupFormTest(SimpleTestCase):
+class MyCustomSignupFormTest(TestCase):
 
     def setUp(self):
         self.form = MyCustomSignupForm()
     
+    def test_user_change_rus_form_fields_label(self):
+        activate('ru')
+        self.assertTrue(self.form.fields['email'].label == None or self.form.fields['email'].label == 'E-mail')
+        self.assertTrue(self.form.fields['password1'].label == None or self.form.fields['password1'].label == 'Пароль')
+        self.assertTrue(self.form.fields['password2'].label == None or self.form.fields['password2'].label == 'Пароль (еще раз)')
+        self.assertTrue(self.form.fields['user_lang'].label == None or self.form.fields['user_lang'].label == 'Язык')
+
+    def test_user_change_eng_form_fields_label(self):
+        activate('en')
+        # print(self.form.fields['password2'].label)
+        self.assertTrue(self.form.fields['email'].label == None or self.form.fields['email'].label == 'E-mail')
+        self.assertTrue(self.form.fields['password1'].label == None or self.form.fields['password1'].label == 'Password')
+        self.assertTrue(self.form.fields['password2'].label == None or self.form.fields['password2'].label == 'Password (again)')
+        self.assertTrue(self.form.fields['user_lang'].label == None or self.form.fields['user_lang'].label == 'Language')
+
+    def test_valid_sign_up_user_eng(self):
+        form_data = {'email': 'a@a.com', 'password1': 'testpass123', 'password2': 'testpass123', 'user_lang': 'en'}
+        form = MyCustomSignupForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data['user_lang'], 'en')
+
+    def test_valid_sign_up_user_rus(self):
+        form_data = {'email': 'a@a.com', 'password1': 'testpass123', 'password2': 'testpass123', 'user_lang': 'ru'}
+        form = MyCustomSignupForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data['user_lang'], 'ru')
+
+    def test_invalid_password_sign_up_user(self):
+        form_data = {'email': 'a@a.com', 'password1': 'testpass123', 'password2': 'poptpass1234', 'user_lang': 'ru'}
+        form = MyCustomSignupForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_language_sign_up_user(self):
+        form_data = {'email': 'a@a.com', 'password1': 'testpass123', 'password2': 'testpass123', 'user_lang': 'br'}
+        form = MyCustomSignupForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertWarnsMessage(form.errors, f'Выберите корректный вариант. br нет среди допустимых значений.')
     
